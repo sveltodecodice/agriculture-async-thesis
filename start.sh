44a09dc3146bd1e2bfe44f5dc9bf8a0645ad46a0
Alessandro <bboh9751@gmail.com>
2026-09-12 15:37:17 +0200

    Add start.sh to automate startup ops

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+echo "==> Stopping and removing previous Docker Compose resources"
+docker compose down \
+  --volumes \
+  --remove-orphans \
+  --rmi local
+
+echo "==> Cleaning Docker build cache."
+docker builder prune --all --force
+
+echo "==> Cleaning unused Docker resources, including volumes"
+docker system prune --all --force --volumes
+
+echo "==> Rebuilding images"
+docker compose build
+
+echo "==> Starting Docker Compose"
+docker compose up "$@"
